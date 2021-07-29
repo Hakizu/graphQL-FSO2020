fc60bb3c0cd88a120ba22774d5bcd2e7e3a6f8a5
Jan Krumrein <hakisuj@gmail.com>
2021-07-29 14:26:02 +0200

    :sparkles: phone edit and filter

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ let persons = [
 ]
 
 const typeDefs = gql`
+  enum YesNo {
+    YES
+    NO
+  }
+
   type Address {
     street: String!
     city: String!
@@ -39,7 +44,7 @@ const typeDefs = gql`
 
   type Query {
       personCount: Int!
-      allPersons: [Person!]!
+      allPersons(phone: YesNo): [Person!]!
       findPerson(name: String!): Person
   }
 
@@ -50,16 +55,30 @@ const typeDefs = gql`
       street: String!
       city: String!
     ): Person
+
+    editNumber(
+      name: String!
+      phone: String!
+    ): Person
   }
 `
 
 const resolvers = {
     Query: {
         personCount: () => persons.length,
-        allPersons: () => persons,
+        allPersons: (root, args) => {
+          if (!args.phone) {
+            return persons
+          }
+
+          const byPhone = (person) =>
+            args.phone === 'YES' ? person.phone : !person.phone
+          return persons.filter(byPhone)
+        },
         findPerson: (root, args) =>
             persons.find(p => p.name === args.name)
     },
+
     Person: {
       address: (root) => {
         return {
@@ -68,6 +87,7 @@ const resolvers = {
         }
       }
     },
+
     Mutation: {
       addPerson: (root, args) => {
         if (persons.find(it => it.name === args.name)) {
@@ -79,6 +99,17 @@ const resolvers = {
         const person = {... args, id: uuid() }
         persons = persons.concat(person)
         return person
+      },
+      editNumber: (root, args) => {
+        const person = persons.find(it => it.name === args.name)
+        if (!person) {
+          return null
+        }
+
+        const updateperson = {...person, phone: args.phone}
+        persons = persons.map(it => it.name === args.name
+          ? updateperson : it)
+        return updateperson
       }
     }
 }
