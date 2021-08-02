ac0a0dfa408e538ca1f475d713744f213031c0a3
Jan Krumrein <hakisuj@gmail.com>
2021-08-02 13:02:27 +0200

    :fire: unused bootstraped stuff

--- follow-along-frontend/src/index.js
+++ follow-along-frontend/src/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache, gql } from '@apollo/client'
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -12,24 +10,6 @@ const client = new ApolloClient({
     uri: 'http://localhost:4000',
   })
 })
-
-// const query = gql`
-// query {
-//   allPersons  {
-//     name,
-//     phone,
-//     address {
-//       street,
-//       city
-//     }
-//     id
-//   }
-// }
-// `
-
-// client.query({ query }).then((response) => {
-//   console.log(response.data)
-// }).catch((error) => console.log(error))
 
 ReactDOM.render(
   <React.StrictMode>
@@ -39,8 +19,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
