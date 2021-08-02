6be1dcaf2478dc465ba1d394292d2f70362ba25c
Jan Krumrein <hakisuj@gmail.com>
2021-08-02 14:05:12 +0200

    :goal_net: error handling on duplicates

--- follow-along-frontend/src/App.jsx
+++ follow-along-frontend/src/App.jsx
@@ -1,21 +1,44 @@
-import {  useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { ALL_PERSONS } from './queries';
 import PersonForm from './PersonForm';
+import React, { useState } from 'react'
 import Persons from './Persons'
-import React from 'react'
 
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(undefined)
+
   const result = useQuery(ALL_PERSONS)
+
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(undefined)
+    }, 10000)
+  }
+
   return (
     <>
+      <Notify errorMessage={errorMessage}/>
       <Persons persons = {result.data.allPersons}/>
-      <PersonForm />
+      <PersonForm setError={notify}/>
     </>
+  )
+}
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null
+  }
+
+  return (
+    <div style={{color: 'red'}}>
+      {errorMessage}
+    </div>
   )
 }
 
