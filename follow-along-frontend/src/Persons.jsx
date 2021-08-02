ceb69e9d81a076d18aed588cacb8a1085ffc2480
Jan Krumrein <hakisuj@gmail.com>
2021-08-02 12:59:12 +0200

    :tada: frontend follow-along

---
+++ follow-along-frontend/src/Persons.jsx
@@ -0,0 +1,58 @@
+import { gql, useLazyQuery } from '@apollo/client';
+import React, { useEffect, useState } from 'react'
+
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`
+
+const Persons = ({ persons }) => {
+  const [ getPerson, result ] = useLazyQuery(FIND_PERSON)
+  const [ person, setPerson ] = useState(undefined)
+
+  const showPerson = (name) => {
+    getPerson({ variables: { nameToSearch: name } })
+  }
+
+    useEffect(() => {
+      if (result.data) {
+        setPerson(result.data.findPerson)
+      }
+    }, [result])
+
+    if (person) {
+      return (
+        <div>
+          <h2>{person.name}</h2>
+          <div>{person.address.street} {person.address.city}</div>
+          <div>{person.phone}</div>
+          <button onClick={() => setPerson(undefined)}>close</button>
+        </div>
+      )
+    }
+
+  return (
+    <div>
+      <h2>Persons</h2>
+        {persons.map(it => 
+          <div key={it.name}>
+            {it.name} {it.phone}
+            <button onClick={() => showPerson(it.name)}>
+              show address
+            </button>
+          </div>
+        )} 
+    </div>
+  )
+}
+
+export default Persons
